f7b8eb3eb1059ccf8bf075fbcd82934b75857d0e
ryosuke.kawashima.career <ryosuke.kawashima.career@gmail.com>
2025-12-10 11:26:23 +0000

    Module4

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,15 +5,24 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.dev/license
  */
-import {Component} from '@angular/core';
+import {Component, signal} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
+import {RecipeModel} from './models';
+import {MOCK_RECIPES} from './mock-recipes';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css',
+  styleUrl: './app.css'
 })
 export class App {
-  protected title = 'myapp';
+  protected readonly recipe = signal<RecipeModel>(MOCK_RECIPES[0]);
+  protected showCarbonara(): void {
+    this.recipe.set(MOCK_RECIPES[0]);
+  }
+  protected showCapreseSalad(): void {
+    this.recipe.set(MOCK_RECIPES[1]);
+  }
 }

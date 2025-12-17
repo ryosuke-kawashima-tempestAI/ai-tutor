7f6f8510e263fde2b0b27f8280849419434989a8
ryosuke.kawashima.career <ryosuke.kawashima.career@gmail.com>
2025-12-17 11:23:59 +0000

    Module9: Refactoring

---
+++ src/app/recipe-detail/recipe-detail.ts
@@ -0,0 +1,39 @@
+import { Component, computed, input, signal } from '@angular/core';
+import { RecipeModel } from '../models';
+import { JsonPipe } from '@angular/common';
+
+@Component({
+  selector: 'app-recipe-detail',
+  imports: [JsonPipe],
+  templateUrl: './recipe-detail.html',
+  styleUrl: './recipe-detail.css',
+})
+export class RecipeDetail {
+  // you receive this from RecipeList
+  protected readonly recipe = input<RecipeModel>();
+  protected readonly servings = signal(1);
+  protected increment(): void {
+    this.servings.update(counter => {
+      return counter + 1;
+    });
+  }
+  protected decrement(): void {
+    this.servings.update(counter => {
+      return counter - 1;
+    });
+  }
+  protected readonly AdjustedIngredients = computed(() => {
+    const originalIngredients = this.recipe()?.ingredients;
+    const curServings = this.servings();
+    const newIngredients = originalIngredients?.map(ingredient => {
+      const adjustedIngredient = {
+        "name": ingredient["name"],
+        "quantity": ingredient["quantity"] * curServings,
+        "unit": ingredient["unit"]
+      };
+      return adjustedIngredient;
+    });
+    // this is the return value of computed function.
+    return newIngredients;
+  });
+}
--- src/app/recipe-list/recipe-list.ts
+++ src/app/recipe-list/recipe-list.ts
@@ -1,42 +1,25 @@
-import { Component, computed, signal } from '@angular/core';
-import { Ingredient, RecipeModel } from '../models';
+import { Component, signal} from '@angular/core';
+import { RecipeModel } from '../models';
 import { MOCK_RECIPES } from '../mock-recipes';
-
+import { RecipeDetail } from '../recipe-detail/recipe-detail';
+// This is the entry point of this component
 @Component({
   selector: 'app-recipe-list',
   standalone: true,
-  imports: [],
+  // Data Transaction
+  imports: [RecipeDetail],
   templateUrl: './recipe-list.html',
   styleUrl: './recipe-list.css',
 })
 export class RecipeList {
   protected readonly recipes: RecipeModel[] = MOCK_RECIPES;
-  protected readonly recipe = signal<RecipeModel>(MOCK_RECIPES[0]);
-  protected readonly servings = signal(0);
+  // This is passed to RecipeDetail.
+  protected readonly activeRecipe = signal<RecipeModel>(MOCK_RECIPES[0]);
 
   protected showCarbonara(): void {
-    this.recipe.set(MOCK_RECIPES[0]);
+    this.activeRecipe.set(MOCK_RECIPES[0]);
   }
   protected showCapreseSalad(): void {
-    this.recipe.set(MOCK_RECIPES[1]);
+    this.activeRecipe.set(MOCK_RECIPES[1]);
   }
-  protected increment(): void {
-    this.servings.update(counter => counter + 1);
-  }
-  protected decrement(): void {
-    this.servings.update(counter => counter - 1);
-  }
-  protected readonly AdjustedIngredients = computed(() => {
-    const originalIngredients = this.recipe().ingredients;
-    const curServings = this.servings();
-    const newIngredients = originalIngredients.map(ingredient => {
-      // returns a JSON object with the same keys as the original object  
-      return {
-        "name": ingredient["name"],
-        "quantity": ingredient["quantity"] * curServings,
-        "unit": ingredient["unit"],
-      };
-    });
-    return newIngredients;
-  })
 }
